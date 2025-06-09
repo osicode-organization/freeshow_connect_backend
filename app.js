5736386b7264c42e32a72d63866730aae3ad6fb6
Osinachi Mbah <osinachi99@gmail.com>
2025-06-09 09:05:36 +0300

    modification

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require(express)
+const express = require('express')
+const frisho = require("freeshow-api")
+
+const { sendHTTP, default: api } = frisho;
 const app = express()
 
 const port = 3000;
 
 app.get('/',(req, res)=>{
+    const API = api("http://localhost:5505")
+    API.sendHTTP("next_slide")
+
     console.log("testinng process")
-    res.send('It is working')
+    res.send('It is wworking')
 })
 
 app.listen(port, ()=>{
