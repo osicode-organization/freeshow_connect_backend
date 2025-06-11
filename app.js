97f54cdeb934c9e9ac2e4a30045f3b66ac70d8d2
Osinachi Mbah <osinachi99@gmail.com>
2025-06-11 18:21:41 +0300

    progress

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const port = 3000;
 app.get('/',async (req, res)=>{
     const API = api("http://localhost:5505")
     API.sendHTTP("get_shows")
-    const apiResponse = await postData("get_show",{"id": "18b53c8b26c"},"http://localhost:5505");
+    // const apiResponse = await postData("get_shows",{},"http://localhost:5505");
+    const apiResponse = await postData("get_show",{"id": "ac7feb2fa36"},"http://localhost:5505");
     
 
     console.log("testinng process")
@@ -20,14 +21,7 @@ const postData = async (action, data, URL) => {
     
     const query = `?action=${action}&data=${JSON.stringify(data)}`
     const res = await fetch(URL + query, { method: "POST", signal: AbortSignal.timeout(100) }).catch(error)
-
-    // const response = await fetch('https://api.example.com/data', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({ name: "Mbah", age: 30 })
-    // });
+    
     const result = await res.json();
     console.log("postData")
     console.log(result);
