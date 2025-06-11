433a2e0aa37cab1b90b73c2d9e649bacc0093234
Osinachi Mbah <osinachi99@gmail.com>
2025-06-11 15:03:09 +0300

    I got show with slide numbers

--- app.js
+++ app.js
@@ -6,13 +6,39 @@ const app = express()
 
 const port = 3000;
 
-app.get('/',(req, res)=>{
+app.get('/',async (req, res)=>{
     const API = api("http://localhost:5505")
-    API.sendHTTP("next_slide")
+    API.sendHTTP("get_shows")
+    const apiResponse = await postData("get_show",{"id": "18b53c8b26c"},"http://localhost:5505");
+    
 
     console.log("testinng process")
-    res.send('It is wworking')
+    res.send(apiResponse)
 })
+
+const postData = async (action, data, URL) => {
+    
+    const query = `?action=${action}&data=${JSON.stringify(data)}`
+    const res = await fetch(URL + query, { method: "POST", signal: AbortSignal.timeout(100) }).catch(error)
+
+    // const response = await fetch('https://api.example.com/data', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ name: "Mbah", age: 30 })
+    // });
+    const result = await res.json();
+    console.log("postData")
+    console.log(result);
+    return result;
+};
+
+function error(err) {
+    if (err.name === "TimeoutError" || err.name === "AbortError") return
+    console.error("Could not connect. Make sure WebSocket/REST is enabled in FreeShow settings>Connection!")
+}
+
 
 app.listen(port, ()=>{
     console.log("server  is running on port ${port}")
